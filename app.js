edcef2f06333493928856bb28c6da40216cd4ac2
kim1037 <pan913063@gmail.com>
2023-03-20 13:27:21 +0800

    feat: show restaurant list

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //require modules
 const express = require("express");
 const exphbs = require("express-handlebars");
+const restaurantList = require("./restaurant.json");
 const app = express();
-const port = 300;
+const port = 3000;
 
 //set view template
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }));
@@ -12,9 +13,9 @@ app.set("view engine", "hbs");
 app.use(express.static("public"));
 
 //set route
-
+//首頁
 app.get("/", (req, res) => {
-  res.send("this is a restarunt recommandation website");
+  res.render("index", { restaurants: restaurantList.results });
 });
 
 app.listen(port, () => {
