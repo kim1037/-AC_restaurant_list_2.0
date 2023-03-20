912de0bb8a9862fba89ecc0b53b7d4634c92e895
kim1037 <pan913063@gmail.com>
2023-03-20 14:41:04 +0800

    feat: connect to mongodb

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 //require modules
 const express = require("express");
 const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
 const restaurantList = require("./restaurant.json");
 const recommendRestaurants = require("./random_restaurant");
 const app = express();
 const port = 3000;
+
+// 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+//connet to mongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+db = mongoose.connection;
+
+//set connect error and success message
+db.on("error", () => {
+  console.log("MongoDB error!");
+});
+
+db.once("open", () => {
+  console.log("Connect success!");
+});
 
 //set view template
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }));
