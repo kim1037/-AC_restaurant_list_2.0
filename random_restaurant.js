906824b16c53cd691404d6bd66442812732cc93b
kim1037 <pan913063@gmail.com>
2023-03-23 16:12:24 +0800

    refactor : restaurant recommend function

--- random_restaurant.js
+++ random_restaurant.js
@@ -1,15 +1,30 @@
-//get random restaurant
+// //get random restaurant
+// function recommendRestaurants(restaurants) {
+//   const randomRestaurantList = [];
+//   let tries = 0; //設置計數器, 用來避免陷入無限迴圈
+//   while (randomRestaurantList.length < 3 && tries < 300) {
+//     const restaurant =
+//       restaurants[Math.floor(Math.random() * restaurants.length)];
+//     if (!randomRestaurantList.some((res) => res._id === restaurant._id)) {
+//       randomRestaurantList.push(restaurant);
+//     }
+//     tries++;
+//   }
+//   return randomRestaurantList;
+// }
+
+//使用set 改善效能
 function recommendRestaurants(restaurants) {
-  const randomRestaurantList = [];
-  let tries = 0; //設置計數器, 用來避免陷入無限迴圈
-  while (randomRestaurantList.length < 3 && tries < 300) {
+  const randomRestaurantSet = new Set();
+  while (
+    randomRestaurantSet.size < 3 &&
+    randomRestaurantSet.size < restaurants.length // 避免餐廳總數<3的時候產生無限迴圈
+  ) {
     const restaurant =
       restaurants[Math.floor(Math.random() * restaurants.length)];
-    if (!randomRestaurantList.some((res) => res._id === restaurant._id)) {
-      randomRestaurantList.push(restaurant);
-    }
-    tries++;
+    randomRestaurantSet.add(restaurant);
   }
-  return randomRestaurantList;
+  return [...randomRestaurantSet];
 }
+
 module.exports = recommendRestaurants;
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,12 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const Restaurant = require("../../models/restaurant");
+const recommendRestaurants = require("../../random_restaurant");
 
 router.get("/", (req, res) => {
   Restaurant.find() //取出model裡的資料
     .lean() //轉換成JS array
     .then((restaurants) => {
       res.render("index", { restaurants });
+    })
+    .catch((e) => console.log(e));
+});
+
+//搜尋結果的route
+router.get("/search", (req, res) => {
+  const keyword = req.query.keyword.trim().toLowerCase();
+  Restaurant.find()
+    .lean()
+    .then((restaurantsData) => {
+      let restaurants = restaurantsData.filter(
+        (restaurant) =>
+          restaurant.name.toLowerCase().includes(keyword) ||
+          restaurant.category.includes(keyword)
+      );
+      const noResults = restaurants.length === 0;
+      //如有搜尋到則維持原本的restaurant array, 沒有執行推薦3間餐廳的function
+      restaurants = restaurants.length
+        ? restaurants
+        : recommendRestaurants(restaurantsData);
+
+      res.render("index", { restaurants, keyword, noResults });
     })
     .catch((e) => console.log(e));
 });
