63bd07653b3582c94d4744c9d107efb259de5f4c
kim1037 <pan913063@gmail.com>
2023-03-24 10:59:32 +0800

    feat: add phone rule in create form

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,14 @@
-//Validation when submit create Form 
+//Validation when submit create Form
 const createButton = document.querySelector("#createButton");
 const createForm = document.querySelector("#createForm");
 createButton.addEventListener("click", function onSubmitButtonClicked(event) {
   createForm.classList.add("was-validated");
 });
+
+// //監聽select事件，更改排序方式
+// const sortBy = document.getElementById("sortBy");
+// sortBy.addEventListener("change", selectChange);
+
+// function selectChange() {
+//   console.log(sortBy.value);
+// }
--- random_restaurant.js
+++ random_restaurant.js
@@ -1,18 +1,3 @@
-// //get random restaurant
-// function recommendRestaurants(restaurants) {
-//   const randomRestaurantList = [];
-//   let tries = 0; //設置計數器, 用來避免陷入無限迴圈
-//   while (randomRestaurantList.length < 3 && tries < 300) {
-//     const restaurant =
-//       restaurants[Math.floor(Math.random() * restaurants.length)];
-//     if (!randomRestaurantList.some((res) => res._id === restaurant._id)) {
-//       randomRestaurantList.push(restaurant);
-//     }
-//     tries++;
-//   }
-//   return randomRestaurantList;
-// }
-
 //使用set 改善效能
 function recommendRestaurants(restaurants) {
   const randomRestaurantSet = new Set();
