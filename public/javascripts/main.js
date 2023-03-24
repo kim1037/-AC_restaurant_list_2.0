6940288965e8d55ac0f7202e3d963645a92cae93
kim1037 <pan913063@gmail.com>
2023-03-24 18:26:25 +0800

    fix : sort bug when search keyword

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,5 +8,13 @@ createButton.addEventListener("click", function onSubmitButtonClicked(event) {
 //透過select，更改餐廳排序方式
 function sortRestaurants() {
   const sortBy = document.querySelector("#sortBy").value;
-  window.location.href = `/?sortBy=${sortBy}`;
+  const regex = /sortBy=.*/;
+  if (window.location.href.includes("search")) {
+    window.location.href = window.location.href.replace(
+      regex,
+      `sortBy=${sortBy}`
+    );
+  } else {
+    window.location.href = `/?sortBy=${sortBy}`;
+  }
 }
